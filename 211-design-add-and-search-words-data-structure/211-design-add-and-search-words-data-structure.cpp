c0c0a2ef4157de683de26d53d54f2bf46599e9fb
omar ayman mahouz <omar.ayman3993@gmail.com>
2022-05-09 15:49:38 +0200

    Time: 1653 ms (35.88%), Space: 623.2 MB (17.71%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,61 @@
+struct Node{
+    vector<Node*> child{26, nullptr};
+    bool isEndOfWord;
+    Node(): isEndOfWord(false){ }
+};
+
+class WordDictionary {
+private:
+    Node* root;
+    bool solve(Node* cur, string& s, int idx){
+        if(idx == s.size() && cur->isEndOfWord) 
+            return true;
+        else if (idx == s.size())
+            return false;
+        if(!cur)
+            return false;
+        
+        if(s[idx] != '.'){
+            if(cur->child[s[idx] - 'a'])
+                return solve(cur->child[s[idx] - 'a'], s, idx + 1);
+            else
+                return false;
+        }
+        else{
+            for(int i = 0; i < 26; i++){
+                if(cur->child[i]){
+                    bool flag = solve(cur->child[i], s, idx + 1);
+                    if(flag)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        Node* temp = root;
+        for (int i = 0; i < word.size(); i++){
+            if(temp->child[word[i] - 'a'])
+                temp = temp->child[word[i] - 'a'];
+            else
+                temp = temp->child[word[i] - 'a'] = new Node();
+        }
+        temp->isEndOfWord = true;
+    }
+    
+    bool search(string word) {
+        return solve(root, word, 0);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
